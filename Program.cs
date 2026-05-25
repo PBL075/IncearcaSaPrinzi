c9768bab5ca6d44233fc4a9ea1006602c2fbe25f
PBL075 <pavelgorea75@gmail.com>
2026-05-25 14:29:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace prinde_error
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+
+            try
+            {
+                Console.WriteLine("Scrieti primul numar");
+            int nr1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Scrieti al doilea numar");
+            int nr2 = Convert.ToInt32(Console.ReadLine());
+            int impartire = nr1 / nr2 ;
+            Console.WriteLine(impartire);
+            }catch (FormatException)
+            {
+                Console.WriteLine("Scrieti numar dar nu litera");
+            }
+            finally
+            {
+                Console.WriteLine("Gata am terminat ");
+            }
+
+        }
+    }
+}
